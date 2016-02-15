e748516698d8c92d74ef1a8c1474e67820741b5d
Johanna Ernst <ernstjojo@aol.com>
2016-02-15 11:00:21 +0000

    Updated Name step counter.

--- app/src/main/java/uk/ac/ox/ibme/androidstepcounter/uk/ac/ox/ibme/androidstepcounter/algos/NamjeStepCounter.java
+++ app/src/main/java/uk/ac/ox/ibme/androidstepcounter/uk/ac/ox/ibme/androidstepcounter/algos/NamjeStepCounter.java
@@ -1,33 +1,69 @@
 package uk.ac.ox.ibme.androidstepcounter.uk.ac.ox.ibme.androidstepcounter.algos;
 
-import java.util.ArrayList;
-import java.util.Queue;
+import android.hardware.SensorManager;
 
 import uk.ac.ox.ibme.androidstepcounter.IStepCounter;
 
 /**
- * Created by engs1397 on 21/01/2016.
+ * An implementation of the Namje step counter.
+ * This step counter using a evolving threshold to estimate step counts.
+ * At the moment the step duration is based on samples and 100 Hz, we could redesign this
+ * to take into account time passed rather than samples in order to take into account
+ * frequency variations in the data collection.
+ * Created by Johanna Ernst
  */
-public class NamjeStepCounter implements IStepCounter {
-    float[] array_of_floats = new float[100];
-    ArrayList<Float> array_list_of_floats = new ArrayList<>();
-
-
+public class ThresholdStepCounter implements IStepCounter {
+    
+    private int clock = 1;
+    private int step = 0;
+    private double alpha = 0.0003;
+    private double beta = 0.35;
+    private double max_val = 0;
+    private double step_dur = 0;
+    
+    // Define limits for step length for 100 Hz signal
+    step_max = 200
+    step_min = 20
+    
+    // Create buffers to store vec_mag information over time
+    //private static final int LONG = 1000;
+    //private static final int SHORT = 50;
+    //private RingBuffer shortBuffer = new RingBuffer(SHORT);
+    //private RingBuffer longBuffer = new RingBuffer(LONG);
+    
     @Override
     public int getSteps(long timestamp, float accx, float accy, float accz) {
-        array_of_floats[1] = 89;
-        int l = array_of_floats.length;
-
-        array_list_of_floats.add(89F);
-        float f = array_list_of_floats.get(1);
-        array_list_of_floats.set(1, 89F);
-        array_list_of_floats.remove(1);
-
-        return 0;
+        double vec_mag = Math.sqrt(Math.pow(accx, 2)+Math.pow(accy, 2)+Math.pow(accz, 2));
+        float thresh = alpha/clock + beta;
+    
+    // This loops checks whether there has been step like behavior, and if so, whether the
+    // step duration is within the 'normal' step limits defined above
+        if ((max_val - vec_mag) >= thresh){
+            step ++;
+            if (step_dur > step_max){
+                step = step-1;
+                step_dur = 0;
+            }else if (step_dur < step_min){
+                step = step-1;
+            }else{
+                clock = 1;
+                max_val = vec_mag;
+            }else if (vec_mag > max_val){
+                max_val = vec_mag;
+            }
+        }
+    
+        step_dur ++;
+        clock ++;
+        
+        return step;
     }
-
+    
     @Override
     public void reset() {
-
+        private int clock = 1;
+        private int step = 0;
+        private double max_val = 0;
+        private double step_dur = 0;
     }
 }
