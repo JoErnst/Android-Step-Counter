905c7f04311936b7375ff03374c2ad8b6feda962
Dario Salvi <dario.salvi@eng.ox.ac.uk>
2016-02-16 18:07:20 +0000

    adding unit tests

---
+++ app/src/test/java/uk/ac/ox/ibme/androidstepcounter/AlgoTests.java
@@ -0,0 +1,20 @@
+package uk.ac.ox.ibme.androidstepcounter;
+
+import org.junit.Test;
+
+import uk.ac.ox.ibme.androidstepcounter.algos.DummyStepCounter;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by engs1397 on 16/02/2016.
+ */
+public class AlgoTests {
+
+    @Test
+    public void testDummyStepCounter(){
+        IStepCounter stepcounter = new DummyStepCounter();
+
+        //stepcounter.getSteps()
+    }
+}
